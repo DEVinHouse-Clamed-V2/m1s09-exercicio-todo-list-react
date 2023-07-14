c0bc764ce5cfd27efbf0fa4dec615177d1a28ba8
Bruno-Costa-fig <brunocosta.fig@gmail.com>
2023-07-14 19:53:58 -0300

    resolução exercicios 01 ao 03

---
+++ src/components/CardAdicionar/index.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+function CardAdicionar(){
+
+  const [textoTarefa, setTextoTarefa] = useState("")
+
+  return (
+    <div>
+      {/* <h1>Texto da tarefa: {textoTarefa}</h1> */}
+      <input type="text" 
+        value={textoTarefa} 
+        onChange={(evento) => setTextoTarefa(evento.target.value)} 
+        placeholder="Digite sua tarefa" 
+      />
+      <button onClick={() => {}}>Adicionar</button>
+    </div>
+  )
+}
+
+export default CardAdicionar
